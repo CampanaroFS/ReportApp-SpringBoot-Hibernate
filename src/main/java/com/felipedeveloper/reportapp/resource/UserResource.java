0ab12077964e22b1b5dc191fa88c478118c76588
Felipe Campanaro <felipe.s.campanaro@gmail.com>
2020-05-08 22:13:39 -0300

    User resource created and findById included

--- src/main/java/com/felipedeveloper/reportapp/resource/UserResource.java
+++ src/main/java/com/felipedeveloper/reportapp/resource/UserResource.java
@@ -1,20 +1,34 @@
 package com.felipedeveloper.reportapp.resource;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.felipedeveloper.reportapp.entities.User;
+import com.felipedeveloper.reportapp.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+	
+	@Autowired
+	private UserService userService;
 
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User user = new User(null, "teste test", "teste@test.com", "Maintenance engineer", 20001000, "123456");
+	public ResponseEntity<List<User>> findAll() {
+		List<User> user = userService.findAll();
 		return ResponseEntity.ok().body(user);
 	}
-
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Optional<User>> findById(@PathVariable Long id){
+		Optional<User> obj = userService.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
 }
